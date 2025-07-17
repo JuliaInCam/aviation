276755e3700b2663e4d88945ab508fb7953fd374
Julia Massabuau <yi241@cam.ac.uk>
2025-07-17 11:52:12 +0100

    format my python code using

--- src/aviation/fleet.py
+++ src/aviation/fleet.py
@@ -1,12 +1,15 @@
-def passengers_per_day(passengers_per_year, days_per_year): 
+def passengers_per_day(passengers_per_year, days_per_year):
     return passengers_per_year / days_per_year
 
-def required_global_fleet(passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day): #this function should take output of the prev funciton as a variable
+
+def required_global_fleet(
+    passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day
+):  # this function should take output of the prev funciton as a variable
     return passengers_per_day / (seats_per_aircraft * flights_per_aircraft_per_day)
+
 
 # required_global_fleet(150.0, 2.0,)
 
 # when defining function, use keyword arguments, otherwise they are positional arguments always have to be given the function in this order!!!
-    # keyword arguments: we assign the variable name to the argument so that the order doesnt matter
-    # def passengers_per_day(*, passengers_per_year, days_per_year = 365.0):  asterisk forces all arguments as keyword arguments, which will initiate keyword agruments for arguments placed after it in the input arguments
-
+# keyword arguments: we assign the variable name to the argument so that the order doesnt matter
+# def passengers_per_day(*, passengers_per_year, days_per_year = 365.0):  asterisk forces all arguments as keyword arguments, which will initiate keyword agruments for arguments placed after it in the input arguments
