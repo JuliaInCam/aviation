9578fa9f0da95091c95efd34a270a6e8aaa7e669
Julia Massabuau <yi241@cam.ac.uk>
2025-07-17 11:03:32 +0100

    refactor automatic cnahges by VSC

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -1,10 +1,10 @@
-import src.aviation.aviation as aviation
+import aviation
 
 days_per_year = 365
 passengers_per_year = 5_000_000_000.0
 seats_per_aricraft = 150.0
 flights_per_aircraft_per_day = 2.0 #make them all floats 
 
-passengers_per_day = aviation. passengers_per_day(passengers_per_year, days_per_year)
+passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
 
 print(f"{passengers_per_day=:,}") #format string syntax, will output variable_name=value
