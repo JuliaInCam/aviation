c6bcc50cb52ba06ebcb3b8ce3454a40330306678
Julia Massabuau <yi241@cam.ac.uk>
2025-07-23 11:22:19 +0100

    adding a bunch of tests

--- analysis/required_global_fleet.py
+++ analysis/required_global_fleet.py
@@ -1,6 +1,8 @@
 """Analysis to determine the required size of the global fleet using transforms defined in `src/aviation`."""
 
 # from aviation import passengers_per_day, required_global_fleet
+import aviation
+from aviation import _engine as engine
 
 days_per_year = 365
 passengers_per_year = 5_000_000_000.0
@@ -8,15 +10,17 @@ seats_per_aircraft = 150.0
 flights_per_aircraft_per_day = 2.0  # make them all floats
 
 
-inputs = {
+inputs = {  # dictionary with strings as keys and floats as values
     "passengers_per_year": passengers_per_year,
     "days_per_year": days_per_year,
     "seats_per_aircraft": seats_per_aircraft,
     "flights_per_aircraft_per_day": flights_per_aircraft_per_day,
 }
 
-output = {"required_global_fleet"}
+output = "required_global_fleet"
 
+systems_model = engine.SystemsModel(aviation.transforms)  # equal to SystemModel Class
+required_global_fleet = systems_model.evaluate(inputs, output)
 
 # passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
 # reassigning function output from aviation file to the variable with the same name
@@ -25,4 +29,4 @@ output = {"required_global_fleet"}
 #    passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day
 # )
 
-# print(f"{required_global_fleet=:.2f}")  # format string syntax, will output variable_name=value
+print(f"{required_global_fleet=:.2f}")  # format string syntax, will output variable_name=value
